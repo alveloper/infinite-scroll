01c0aa955b147dde9c327fe973c9c2b7bbff45c0
alveloper <alveloper@gmail.com>
2021-06-28 17:03:19 +0900

    chore: install and apply tailwind css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 const App = () => {
   return (
     <>
+      <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
+        Hello, Tailwind CSS!
+      </button>
     </>
   );
-}
+};
 
 export default App;
