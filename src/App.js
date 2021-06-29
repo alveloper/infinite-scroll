497b8cd736f25378fb1b80d60d186a1030792541
alveloper <alveloper@gmail.com>
2021-06-29 15:14:06 +0900

    Feat: create header, search bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
+import Main from './main/Main';
+
 const App = () => {
   return (
     <>
-      <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
-        Hello, Tailwind CSS!
-      </button>
+    <Main />
     </>
   );
 };
