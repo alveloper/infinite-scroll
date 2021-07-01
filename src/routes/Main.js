7a50b2f35a05198eeebd138611dd5067eee15c48
alveloper <alveloper@gmail.com>
2021-07-01 20:40:19 +0900

    Refac: make SearchBar.js component

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
 
 const Main = () => {
   const [query, setQuery] = useState("");
   const [filteredPosts, setFilteredPosts] = useState([]);
-
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(0);
   const [loading, setLoading] = useState(false);
@@ -54,6 +54,11 @@ const Main = () => {
     }
   }, [loading, num]);
 
+  // 필터링: SearchBar.js -> Main.js 로 데이터를 넘기기 위해 사용할 함수.
+  const handleQueryChange = (newQuery) => {
+    setQuery(newQuery);
+  };
+
   // https://recruit-api.yonple.com/recruit/251825/a-posts?search=rerum -> rerum 결과만 나옴
   const fetchFilteredPosts = async (query) => {
     await axios
@@ -91,38 +96,7 @@ const Main = () => {
               </p>
             </div>
             {/* SEARCH BAR */}
-            <form className="relative mb-5 text-gray-500">
-              <label
-                htmlFor="search"
-                className="absolute inset-y-0 left-0 flex items-center pl-2"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="inline w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </label>
-              <input
-                id="search"
-                type="search"
-                className="w-full mt-1 text-black border-gray-300 rounded-md shadow-sm pl-9 focus:border-indigo-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-30"
-                placeholder="검색어를 입력하세요"
-                onChange={(e) => {
-                  setQuery(e.target.value);
-                  console.log(query);
-                }}
-                value={query}
-              />
-            </form>
+            <SearchBar handleQueryChange={handleQueryChange} />
             {/* BOARD */}
             {!query ? (
               <>
@@ -130,7 +104,6 @@ const Main = () => {
                   const id = post.id;
                   const title = post.title;
                   const content = post.content;
-
                   return (
                     <div
                       key={index}
